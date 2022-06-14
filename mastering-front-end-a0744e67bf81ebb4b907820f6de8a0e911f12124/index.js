c38a64569914e9e40742c5e884ee84b6183ed94b
piwiit <bouillaux@hotmail.fr>
2022-06-14 23:17:57 +0200

    add sound file and finish logic js

--- mastering-front-end-a0744e67bf81ebb4b907820f6de8a0e911f12124/index.js
+++ mastering-front-end-a0744e67bf81ebb4b907820f6de8a0e911f12124/index.js
@@ -1,20 +1,31 @@
 // Créer la logique de compte à rebours
 let totalSeconds;
+let myInterval;
+const audio = new Audio('sonnerie.wav');
 
 const showCountdown = () => {
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
 
-  seconds.toLocaleString(undefined, {
-    minimumIntegerDigits: 2,
-  });
+  countdownDisplay.innerText = `${minutes} : ${seconds > 10 ? seconds : '0' + seconds}`;
 
-  countdownDisplay;
-  console.log(minutes, seconds);
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    clearInterval(myInterval);
+    countdownDisplay.innerText = "C'est terminé";
+    audio.play();
+  }
+  console.log(totalSeconds);
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  showCountdown();
+  totalSeconds = choice.value * 60;
+  choice.value = '';
+  audio.pause();
+  audio.currentTime = 0;
+  clearInterval(myInterval);
+  myInterval = setInterval(showCountdown, 100);
 });
